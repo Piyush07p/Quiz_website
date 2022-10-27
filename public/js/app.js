bca989c98fc8cec9452c8a278f3e45cc3bc1d3cd
piyush patel <piyush007@gmail.com>
2022-10-27 15:55:19 +0530

    some more questions added

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 
-
-export let  q1=[{
+// -------------networking_question---------------
+ let  q1=[{
 
     'que':'what is the full form of smtp',
     'a':'a) simple mail testing protocol',
@@ -209,5 +209,202 @@ export let  q1=[{
 }
 ]
 
+
+
+// ---------------reasoning_question------------------
+
+// ------------(1)-coding-decoding---------------
+
+ const q2=[
+    {
+      'que':'if 5+3+2=151022, 9+2+4=183652, 8+6+3=482466 then 7+2+5= ?',
+      'a':'a) 143547',
+      'b':'b) 132234',
+      'c':'c) 2577224',
+      'd':'d) 112321',
+      'correct':0
+    },
+    {
+       'que':'If in a certain code "RANGE" is coded as 12345 and "RANDOM" is coded as 123678 then "MANGO" would be',
+       'a':'a) 89343',
+       'b':'b) 84629',
+       'c':'c) 82347',
+       'd':'d) 83274',
+       'correct':2
+    },
+    {
+        'que':'COMPUTER is written as RFUVQNPC. How will MEDICINE be written in that code langauge',
+        'a':'a) MFEDJJOE',
+        'b':'b) EOJDEJFM',
+        'c':'c) MFEJDJOE',
+        'd':'d) EOJDJEFM',
+        'correct':3
+     },
+     {
+        'que':'If 9X7=3545 and 4X3=1520 then 6X8=?',
+        'a':'a) 5040',
+        'b':'b) 6050',
+        'c':'c) 4030',
+        'd':'d) 3040',
+        'correct':2
+     },
+     {
+        'que':'BEAN =ABNE  and SALE=LSEA then NEWS=?',
+        'a':'a) NWES',
+        'b':'b) WNSE',
+        'c':'c) ESWN',
+        'd':'d) SWNE',
+        'correct':1
+     },
+     {
+        'que':'FIG : EGHJFH :: BIN : ?',
+        'a':'a) CAJHOM',
+        'b':'b) CAHJMOC',
+        'c':'c) ACJHMO',
+        'd':'d) ACHJMO',
+        'correct':3
+     },
+     {
+        'que':'CAT=3120 and DOG=4157 then 25144=?',
+        'a':'a) BEND',
+        'b':'b) BEADD',
+        'c':'c) YADD OR YND',
+        'd':'d) can`t  be determined',
+        'correct':3
+     },
+     {
+        'que':'If A=26, DOT=42 then CONE=?',
+        'a':'a) 52',
+        'b':'b) 71',
+        'c':'c) 67',
+        'd':'d) none',
+        'correct':1
+     },
+     {
+        'que':'If 264*2=6, 870*3=11 then 735*5=?',
+        'a':'a) 8',
+        'b':'b) 21',
+        'c':'c) 12',
+        'd':'d) 18',
+        'correct':2
+     },
+     {
+        'que':'If MOUSE=41 and RATE=6 then NETWORK=?',
+        'a':'a) 20',
+        'b':'b) 25',
+        'c':'c) 55',
+        'd':'d) 60',
+        'correct':0
+     },
+     {
+        'que':'Z,U,Q, _ , L',
+        'a':'a) M',
+        'b':'b) N',
+        'c':'c) O',
+        'd':'d) I',
+        'correct':1
+     }
+]
+
+
+
+// -----------(2)-blood_relation-----------------
+
+let q3=[
+    {
+        'que':'Introducing a boy ,Amarjeet says, "HE is the son of the daughter of my father".How is boy related to the Amarjeet .',
+        'a':'a) Nephew',
+        'b':'b) Maternal uncle',
+        'c':'c) Son',
+        'd':'d) cannot be determined',
+        'correct':3
+     },
+     {
+        'que':'Introducing a boy Ankit said,"he is the son of daughter of my parental grandfather`s son.How is that boy related to ankit ',
+        'a':'a) cousin',
+        'b':'b) brother',
+        'c':'c) father-in-law',
+        'd':'d) Nephew',
+        'correct':3
+     },
+     {
+        'que':'Introducing a girl ,Sangeeta says,"She is the daughter of the brother-in-law of my husband.How is the girl related to daughter.',
+        'a':'a) Niece',
+        'b':'b) Daughter',
+        'c':'c) Sister',
+        'd':'d) Cousin',
+        'correct':0
+     },
+     {
+        'que':'P and Q are sister. R and S are brother`s. P`s daughter is R`s sister.What is Q`s relation to S.',
+        'a':'a) Mother',
+        'b':'b) Grandmother',
+        'c':'c) Aunt',
+        'd':'d) Sister',
+        'correct':2
+     },
+     {
+        'que':'Raman is Sunaina`s father and Sunaina is Vidur`s wife. Saksham is the  father of Vidur`s father.How is Saksham realted to Sunaina.',
+        'a':'a) Father-in-law',
+        'b':'b) Grandfather-in-law',
+        'c':'c) Son',
+        'd':'d) Husband',
+        'correct':1
+     },
+     {
+        'que':'Introducing a girl a man says, She is the daughter of the daughter of my wife. How is man related to the girl.',
+        'a':'a) Maternal uncle',
+        'b':'b) Father',
+        'c':'c) Son',
+        'd':'d) Maternal grandfather',
+        'correct':3
+     },
+     
+     {
+        'que':'Raman is Sunaina`s father and Sunaina is Vidur`s wife. Saksham is the  father of Vidur`s father.How is Saksham realted to Sunaina.',
+        'a':'a) Father-in-law',
+        'b':'b) Grandfather-in-law',
+        'c':'c) Son',
+        'd':'d) Husband',
+        'correct':1
+     },
+     {
+        'que':'Raman is Sunaina`s father and Sunaina is Vidur`s wife. Saksham is the  father of Vidur`s father.How is Saksham realted to Sunaina.',
+        'a':'a) Father-in-law',
+        'b':'b) Grandfather-in-law',
+        'c':'c) Son',
+        'd':'d) Husband',
+        'correct':1
+     },
+     {
+        'que':'Raman is Sunaina`s father and Sunaina is Vidur`s wife. Saksham is the  father of Vidur`s father.How is Saksham realted to Sunaina.',
+        'a':'a) Father-in-law',
+        'b':'b) Grandfather-in-law',
+        'c':'c) Son',
+        'd':'d) Husband',
+        'correct':1
+     },
+     {
+        'que':'Raman is Sunaina`s father and Sunaina is Vidur`s wife. Saksham is the  father of Vidur`s father.How is Saksham realted to Sunaina.',
+        'a':'a) Father-in-law',
+        'b':'b) Grandfather-in-law',
+        'c':'c) Son',
+        'd':'d) Husband',
+        'correct':1
+     }
+
+]
+
+let get
+const btn=document.querySelectorAll('.btn')
+
+Array.from(btn).forEach(elem =>{
+    elem.addEventListener('click',()=>{
+        get=elem.dataset.btn
+        console.log(get)
+    })
+})
+
+export let api=q3
 
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,104 +1,13 @@
 
-// import {question} from "./app.js";
-const question=[
-    {
-      'que':'if 5+3+2=151022, 9+2+4=183652, 8+6+3=482466 then 7+2+5= ?',
-      'a':'a) 143547',
-      'b':'b) 132234',
-      'c':'c) 2577224',
-      'd':'d) 112321',
-      'correct':0
-    },
-    {
-       'que':'If in a certain code "RANGE" is coded as 12345 and "RANDOM" is coded as 123678 then "MANGO" would be',
-       'a':'a) 89343',
-       'b':'b) 84629',
-       'c':'c) 82347',
-       'd':'d) 83274',
-       'correct':2
-    },
-    {
-        'que':'COMPUTER is written as RFUVQNPC. How will MEDICINE be written in that code langauge',
-        'a':'a) MFEDJJOE',
-        'b':'b) EOJDEJFM',
-        'c':'c) MFEJDJOE',
-        'd':'d) EOJDJEFM',
-        'correct':3
-     },
-     {
-        'que':'If 9X7=3545 and 4X3=1520 then 6X8=?',
-        'a':'a) 5040',
-        'b':'b) 6050',
-        'c':'c) 4030',
-        'd':'d) 3040',
-        'correct':2
-     },
-     {
-        'que':'BEAN =ABNE  and SALE=LSEA then NEWS=?',
-        'a':'a) NWES',
-        'b':'b) WNSE',
-        'c':'c) ESWN',
-        'd':'d) SWNE',
-        'correct':1
-     },
-     {
-        'que':'FIG : EGHJFH :: BIN : ?',
-        'a':'a) CAJHOM',
-        'b':'b) CAHJMOC',
-        'c':'c) ACJHMO',
-        'd':'d) ACHJMO',
-        'correct':3
-     },
-     {
-        'que':'CAT=3120 and DOG=4157 then 25144=?',
-        'a':'a) BEND',
-        'b':'b) BEADD',
-        'c':'c) YADD OR YND',
-        'd':'d) can`t  be determined',
-        'correct':3
-     },
-     {
-        'que':'If A=26, DOT=42 then CONE=?',
-        'a':'a) 52',
-        'b':'b) 71',
-        'c':'c) 67',
-        'd':'d) none',
-        'correct':1
-     },
-     {
-        'que':'If 264*2=6, 870*3=11 then 735*5=?',
-        'a':'a) 8',
-        'b':'b) 21',
-        'c':'c) 12',
-        'd':'d) 18',
-        'correct':2
-     },
-     {
-        'que':'If MOUSE=41 and RATE=6 then NETWORK=?',
-        'a':'a) 20',
-        'b':'b) 25',
-        'c':'c) 55',
-        'd':'d) 60',
-        'correct':0
-     },
-     {
-        'que':'Z,U,Q, _ , L',
-        'a':'a) M',
-        'b':'b) N',
-        'c':'c) O',
-        'd':'d) I',
-        'correct':1
-     },
-     {
-        'que':'CASUAL is written as "111914161956" REFER="1834723418" then REFUSAL=? ',
-        'a':'a) 18347216145613',
-        'b':'b) 187234161411956',
-        'c':'c) 18347216141956',
-        'd':'d) none',
-        'correct':2
+import {api} from "./app.js";
 
-     }
-]
+
+let question=[];
+
+for(let i=0;i<api.length;i++)
+{
+    question.push(api[i]);
+}
  
 
 let ans;
@@ -158,7 +67,8 @@ function count_time(){
 
 const quiz_box=document.querySelector('.quiz')
 const timer=document.querySelector('.timer')
-function startQ(){
+
+start_btn.onclick=()=>{
       
       quiz_box.style.display="unset";
       start.style.display="none";
@@ -176,20 +86,21 @@ function startQ(){
 // ------------------choose_options-------------------
 
 var li=document.querySelectorAll('li');
-function changeC(x){
-    
-     for(let i=0;i<4;i++)
+
+Array.from(li).forEach(elem =>{
+    elem.addEventListener('click',()=>{
+        for(let i=0;i<4;i++)
      {
          if(li[i].classList.contains('color'))
          li[i].classList.remove('color')
      }
-    li[x].classList.add('color');
-    if(ans==x){
+    li[elem.dataset.li].classList.add('color');
+    if(ans==elem.dataset.li){
         score++;
     }
     checked=true;
-    
-}
+    })
+})
 
 
 
